8fe7f84e25f0f5abfdb7f88946026cbe916e173d
AsimSheraz12 <1240100471@uskt.edu.pk>
2026-09-12 15:28:55 +0500

    Make Logics And Fanalize Game

--- game-Logic.js
+++ game-Logic.js
@@ -6,26 +6,31 @@ let mode = "Light";
 
     bttnMode.addEventListener("click", () => {
        if(mode === "Light"){
-    bttnMode.addEventListener("click", () => {
         body.style.backgroundColor = "black";
         para.style.color = "white";
-    })
+  
     mode = "Dark";
     console.log(mode);
 }
 else{
-    bttnMode.addEventListener("click", () => {
         body.style.backgroundColor = "White";
         para.style.color = "black";
-    })
+  
     mode = "Light";
 }
     })
 
     let userScore = 0;
     let compScore = 0;
+    let drawgame = 0;
+    let totalgame = 0;
 
     let choices = document.querySelectorAll(".choice");
+    const message = document.querySelector("#mesContainer");
+    let usercount = document.querySelector("#userScore");
+    let compcount = document.querySelector("#compScore");
+    let total = document.querySelector("#total");
+    let draw = document.querySelector("#draw");
 
     const gencompchoice = () =>{
         const choices = ["rock","paper","scissor"];
@@ -34,22 +39,69 @@ else{
         return(compchoice);
     }
 
-    let playgame = (userchoice1) => {
-        console.log("user chooice is " + userchoice1);
-        const compchoice = gencompchoice();
-        // console.log("user comp is " + compchoice);
-        if(userchoice1 === compchoice){
-
-            console.log(`User Choice is ${userchoice1} and Computer Choice is ${compchoice}`);
-            // console.log("Game Draw");
-        }
-    }
-
     choices.forEach((choice) => {
         choice.addEventListener("click", () =>{
+            totalgame ++;
+            total.innerText = totalgame;
+            // message.innerText = "Choose Your Move";
             let userchoice = choice.getAttribute("id");
             // console.log(choice.getAttribute("id"));
             // console.log(userchoice);
             playgame(userchoice);
         });
     });
+
+    let playgame = (userchoice1) => {
+        
+        // console.log("user chooice is " + userchoice1);
+        const compchoice = gencompchoice();
+        // console.log("user comp is " + compchoice);
+        if(userchoice1 === compchoice){
+
+            // console.log(`User Choice is ${userchoice1} and Computer Choice is ${compchoice}`);
+            drawGame();
+            // console.log("Game is Drawn");
+        }
+        else{
+            let winuser = true;
+            if(userchoice1 === "rock"){
+                winuser = compchoice === "paper" ? false : true ;
+            }
+            else if(userchoice1 === "paper"){
+                winuser = compchoice === "scissor" ? false : true ;
+            }
+            else{
+                winuser = compchoice === "rock" ? false : true ;
+            }
+
+            showuserwin(winuser);
+        }
+    }
+
+    const drawGame = () => {
+        drawgame ++;
+        console.log("Game is Draw");
+        message.innerText = "Match Drawn Choose next Move";
+        message.style.backgroundColor = "white";
+        message.style.color = "black";
+        draw.innerText = drawgame;
+    }
+
+    const showuserwin = (userwin) => {
+        if(userwin){
+            userScore ++;
+            console.log("You Win");
+            message.innerText = "You win Choose next Move";
+            message.style.backgroundColor = "green";
+            message.style.color = "white";
+            usercount.innerText = userScore;
+        }
+        else{
+            compScore ++;
+            console.log("You Lose");
+            message.innerText = "You Lose Choose next Move";
+            message.style.backgroundColor = "red";
+            message.style.color = "white";
+            compcount.innerText = compScore;
+        }
+    }
